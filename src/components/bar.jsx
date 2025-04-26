1b9f6eaf69850560b488995a7ebb4c09098d49cb
Muhammad Ali <alikhancss27@gmail.com>
2025-04-26 14:25:24 +0500

    Update bar.jsx

--- src/components/bar.jsx
+++ src/components/bar.jsx
@@ -1,26 +1,26 @@
-
 import * as React from 'react';
 import { BarChart } from '@mui/x-charts/BarChart';
+import Box from '@mui/material/Box';
 
-export default function SimpleCharts({pendingData,totalData,approvedData}) {
+export default function SimpleCharts({ pendingData, totalData, approvedData }) {
   return (
-    <BarChart
-      xAxis={[
-        {
-          id: 'barCategories',
-        data: ['PENDING LOAN', 'TOTAL REQUEST', 'APPROVED LOAN'],
-          scaleType: 'band',
-        },
-      ]}
-      series={[
-        {
-          data: [pendingData, totalData, approvedData],
-           color: '#1a011f'
-        },
-      ]}
-      height={300}
-    />
+    <Box sx={{ width: '100%', height: { xs: 200, sm: 250, md: 400 },}}>
+      <BarChart
+        xAxis={[
+          {
+            id: 'barCategories',
+            data: ['PENDING', 'TOTAL', 'APPROVED'],
+            scaleType: 'band',
+          },
+        ]}
+        series={[
+          {
+            data: [pendingData, totalData, approvedData],
+            color: '#1a011f',
+          },
+        ]}
+        height={300} 
+      />
+    </Box>
   );
 }
-    
-    
